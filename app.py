ab1cfd455f3789fef8ea922234df1d6e4b185793
Vasil Vanchuk <v.vanchuk@tut.by>
2020-01-14 21:49:08 +0200

    Extract _base.j2 template

--- app.py
+++ app.py
@@ -8,38 +8,49 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template(
-        "index.html", directions=data.get_directions(), tours=data.get_tours()
+        "index.j2",
+        tours=data.get_tours(),
+        menu_departures=data.get_menu_departures(),
     )
 
 
-@app.route("/from/<direction>")
+@app.route("/from/<direction>/")
 def from_direction(direction):
-    direction = data.get_direction_info(direction)
+    tours=data.get_tours(direction=direction),
 
     if not direction:
         abort(404)
 
     return render_template(
-        "from_direction.html",
-        directions=data.get_directions(),
+        "from_direction.j2",
         direction=direction,
         tours=data.get_tours(direction=direction),
+        menu_departures=data.get_menu_departures(),
     )
 
 
-@app.route("/tours/<tour_id>")
+@app.route("/tours/<tour_id>/")
 def tour(tour_id):
     tour = data.get_tour(tour_id)
 
     if not tour:
         abort(404)
 
-    return render_template("tour.html", directions=data.get_directions(), tour=tour)
+    return render_template(
+        "tour.j2",
+        tour=tour,
+        menu_departures=data.get_menu_departures(),
+    )
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("404.html", title="404"), 404
+    return (
+        render_template(
+            "404.j2", title="404", menu_departures=data.get_menu_departures()
+        ),
+        404,
+    )
 
 
 if __name__ == "__main__":
--- data.py
+++ data.py
@@ -1,13 +1,27 @@
 import mock_data as mock_data
 
-def get_directions():
-    pass
-
 def get_tours(direction=None):
-    pass
+  tours = map(lambda key: {'id': key, **mock_data.tours[key] } ,mock_data.tours.keys())
+
+  if not direction:
+    return tours
+
+  return list(filter(lambda tour: tour['departure'] == direction, tours))
+
 
 def get_direction_info(direction):
     pass
 
+
 def get_tour(tour_id):
-    pass
+  tour = mock_data.tours[int(tour_id)]
+
+  if tour:
+    return {
+      'id': tour_id,
+      **tour  
+    }
+
+
+def get_menu_departures():
+    return mock_data.departures
